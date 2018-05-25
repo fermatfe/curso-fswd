3358f6f25f103913af2e32cc2bbb311d6d9e578e
fermatfe <fermatfe@jcyl.es>
2018-05-25 13:47:28 +0200

    Clase principal

---
+++ src/main/java/es/jcyl/abcd/efgh/FullstackwebdevApplication.java
@@ -0,0 +1,13 @@
+package es.jcyl.abcd.efgh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackwebdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackwebdevApplication.class, args);
+	}
+	
+}

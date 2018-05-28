c19c906119d3f1509a23fad0b3363aceabfff76d
fermatfe <fermatfe@jcyl.es>
2018-05-28 12:40:10 +0200

    SoloLectura migrado

--- src/main/java/es/jcyl/abcd/efgh/persistencia/repositorios/SoloLecturaRepositorio.java
+++ src/main/java/es/jcyl/abcd/efgh/persistencia/repositorios/SoloLecturaRepositorio.java
@@ -4,17 +4,14 @@ import java.io.Serializable;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-
 @NoRepositoryBean
-public interface SoloLecturaRepositorio<T, ID extends Serializable> extends PagingAndSortingRepository <T, ID> {
-	
-	T findOne ( ID id );
-	
-	Iterable<T> findAll ();
-	
-	Page<T> findAll(Pageable pageable);
+public interface SoloLecturaRepositorio<T, ID extends Serializable> extends PagingAndSortingRepository<T, ID> {
 
+	Iterable<T> findAll(Sort sort);
+
+	Page<T> findAll(Pageable pageable);
 }

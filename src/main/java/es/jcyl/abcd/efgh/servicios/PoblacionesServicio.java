b99ce900f79c8e431b1ca9cd4b240e33b87a2697
fermatfe <fermatfe@jcyl.es>
2018-06-06 18:32:00 +0200

    Interfaces de servicios

---
+++ src/main/java/es/jcyl/abcd/efgh/servicios/PoblacionesServicio.java
@@ -0,0 +1,12 @@
+package es.jcyl.abcd.efgh.servicios;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import es.jcyl.abcd.efgh.persistencia.entidades.PoblacionEntidad;
+
+public interface PoblacionesServicio {
+
+	Page<PoblacionEntidad> getPagina(Integer provinciaId, String pobl, Pageable pageable);
+
+}
---
+++ src/main/java/es/jcyl/abcd/efgh/servicios/ProvinciasServicio.java
@@ -0,0 +1,11 @@
+package es.jcyl.abcd.efgh.servicios;
+
+import java.util.List;
+
+import es.jcyl.abcd.efgh.persistencia.entidades.ProvinciaEntidad;
+
+public interface ProvinciasServicio {
+
+	List<ProvinciaEntidad> getListado(String prov);
+
+}

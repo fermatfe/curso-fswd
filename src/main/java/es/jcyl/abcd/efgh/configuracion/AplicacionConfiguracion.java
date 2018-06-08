97acba38788091fd4fb1c288e2ce936f25ecee9d
fermatfe <fermatfe@jcyl.es>
2018-06-08 13:58:52 +0200

    Práctica 3, CORS activado de manera global

--- src/main/java/es/jcyl/abcd/efgh/configuracion/AplicacionConfiguracion.java
+++ src/main/java/es/jcyl/abcd/efgh/configuracion/AplicacionConfiguracion.java
@@ -1,12 +1,31 @@
 package es.jcyl.abcd.efgh.configuracion;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableJpaRepositories("es.jcyl.abcd.efgh.persistencia")
 @EnableTransactionManagement
 public class AplicacionConfiguracion {
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/v1/**")
+						.allowedOrigins("http://dominio.com")
+						.allowedMethods("GET", "PUT", "POST")
+						.allowedHeaders("content-type", "accept")
+						.exposedHeaders("x-requested-with")
+						.allowCredentials(true)
+						.maxAge(3600);
+			}
+		};
+	}
+	
 }
